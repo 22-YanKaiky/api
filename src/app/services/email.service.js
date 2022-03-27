4317b45fbde4aaa9217c1d76d7fe3390b291e329
22-YanKaiky <yankaikys@gmail.com>
2022-03-27 17:05:41 -0300

    feat(email.service): html

--- src/app/services/email.service.js
+++ src/app/services/email.service.js
@@ -17,22 +17,13 @@ class EmailService {
     });
 
     const mail = {
-      from: '"© 2022, Cinemovie" <yankaiky@outlook.com>',
+      from: '"Cinemovie" <yankaiky@outlook.com>',
       to: email,
       subject: "Bem vindo Nemonauta",
       text: "Você agora têm acesso ao site Cinemovie - Filmes, Séries e Animes gratuitos",
-      html: '<div><h4>Chegou filme novo</h4><h5>The Batman</h5><img src="https://www.nerdsite.com.br/wp-content/uploads/2022/02/Imagem-Destaca-NS-1-1.jpg" width="100%" height="38%"/><a href="https://cinemovie-web.netlify.app/">Só clicar e assistir</a></div>',
     };
 
-    const message = (e) => {
-      if (e) {
-        console.log(e);
-      } else {
-        console.log("Email has sent");
-      }
-    };
-
-    await transporter.sendMail(mail, message);
+    transporter.sendMail(mail);
   }
 }
 
